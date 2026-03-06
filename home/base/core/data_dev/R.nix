8f3c303207217eee747fbeab55f723f425186041
Shey Kail <sheykail132@gmail.com>
2026-03-06 19:00:36 +0800

    fix broken R package

--- home/base/core/data_dev/R.nix
+++ home/base/core/data_dev/R.nix
@@ -1,11 +1,21 @@
 {
   pkgs,
   ...
-}: with pkgs;
-let
-  # Override httpgd to mark it as not broken
-  R-with-my-packages = rWrapper.override{
-    packages = with rPackages; [
+}: let
+  # Create a local pkgs instance that allows broken packages
+  # This is much cleaner than a global nixpkgs.config.allowBroken = true;
+  # because it only affects THIS specific R environment.
+  pkgs-allow-broken = import pkgs.path {
+    system = pkgs.stdenv.hostPlatform.system;
+    config = pkgs.config // {
+      allowBroken = true;
+    };
+    # We should also pass overlays if they are needed, 
+    # but here we can just use the base packages since we want unigd.
+  };
+
+  R-with-my-packages = pkgs-allow-broken.rWrapper.override {
+    packages = with pkgs-allow-broken.rPackages; [
       tidyverse
       tidytable
       clusterProfiler
