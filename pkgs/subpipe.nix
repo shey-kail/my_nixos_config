3a025cf42ac04364264fb4d855f1c10be0868152
Shey Kail <sheykail132@gmail.com>
2026-05-22 20:48:59 +0800

    update subpipe

--- pkgs/subpipe.nix
+++ pkgs/subpipe.nix
@@ -5,15 +5,14 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "subpipe";
-  version = "2.1.2";
+  version = "2.2.2";
 
   src = fetchFromGitHub {
     owner = "shey-kail";
     repo = "subpipe";
     rev = "v${version}";
-    hash = "sha256-2ZL0cnC3EK/jZqRCXwcmkQ3nqIiVMDNC63asQy4MoIs=";
+    hash = "sha256-ZjJ345c4zjX1FsJKz7gqyDIQZqgi3ewf6TTrsYZ8RbQ=";
   };
-
   cargoHash = "sha256-MtoAaE/vy58xpEqzzI9fvAi76+Vy6L9L7/PfQqNqXsk=";
 
   doCheck = false;
