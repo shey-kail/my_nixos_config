d052b43bbc5566daebe83b80f285c9c3f54fc943
Shey Kail <sheykail132@gmail.com>
2026-08-28 13:05:01 +0800

    fix obs build bug

--- home/linux/gui/base/creative.nix
+++ home/linux/gui/base/creative.nix
@@ -1,7 +1,8 @@
 {
   pkgs,
   ...
-}: {
+}:
+{
   programs = {
     # live streaming
     obs-studio = {
@@ -21,13 +22,11 @@
         input-overlay
         obs-multi-rtmp
         obs-source-clone
-        obs-shaderfilter
         obs-source-record
         obs-livesplit-one
         looking-glass-obs
         obs-vintage-filter
         obs-command-source
-        obs-move-transition
         obs-backgroundremoval
         # advanced-scene-switcher
         obs-pipewire-audio-capture
